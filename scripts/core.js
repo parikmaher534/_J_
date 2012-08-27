5217eee385fe86b20155ad96e0b4ba3f1811eb03
Nick <parikmaher534@gmail.com>
2012-08-27 11:50:00 +0300

    Create common js structure( namespace, objects stack, api )
    Create common js structure( namespace, objects stack, api )

--- scripts/core.js
+++ scripts/core.js
@@ -1,60 +1,46 @@
-if( WEBGL == undefined ) var WEBGL = {};
+var _J_ = (function(){
+  
+  /**
+   * Private variables and methods
+   */
+  var th_    = this,
+      conf   = _J_Config,
+      lang   = _J_Lang,
+      canvas = null,
+      ctx    = null;
+  
+  
+  /**
+   * ERROR message
+   */
+  var ERROR = function(e){console.log(e);};
+  
+  
+  /**
+   * Canvas initialization point
+   */
+  ;(function(c, l){
+    window.addEventListener("load", function(){
+      canvas = document.getElementById(c.canvasId);
+      if( canvas ){
+        if( canvas.getContext ){
+          ctx = canvas.getContext("2d");
+        }else{
+          ERROR(l.noctx);
+        };
+      }else{
+        ERROR(l.nocanvas);
+      };
+    }, false);
+  }(conf, lang));
+  
+  
+  
+  /**
+   * return library API
+   */
+  return {
 
-WEBGL.canvas = null;
-WEBGL.ctx    = null;
-
-
-/**
- * WEBGL plot initialization
- */
-WEBGL.init = function(plotName){
-  WEBGL.canvas = document.getElementById(plotName);
-  try{
-    WEBGL.ctx = WEBGL.canvas.getContext("experimental-webgl");
-  }catch(e){
-    document.body.innerHTML = "You don't support WEBGL!";
   };
-};
-
-
-/**
- * WEBGL create buffer
- */
-WEBGL.buffer = function(){
-  var _object_buffer = WEBGL.ctx.createBuffer(),
-  vertexes = [
-    0.0, 1.0, 0.0,
-    -1.0, -1.0, 0.0,
-    1.0, -1.0, 0.0
-  ];
   
-  WEBGL.ctx.bindBuffer(WEBGL.ctx.ARRAY_BUFFER, _object_buffer);
-  WEBGL.ctx.bufferData(WEBGL.ctx.ARRAY_BUFFER, new Float32Array(vertexes), WEBGL.ctx.STATIC_DRAW);
-};
-
-
-/**
- * WEBGL shaders
- */
-WEBGL.shader = function(){
-  
-};
-
-
-/**
- * WEBGL draw scene
- */
-WEBGL.draw = function(){
-  
-};
-
-
-
-window.onload = function(){
-  WEBGL.init("canvas");
-  WEBGL.buffer();
-  
-  setInterval(function(){
-     WEBGL.draw();
-  },100);
-};
+}());
