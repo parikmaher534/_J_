47049ea1bbd91ec2b160099e7c4f80eee87a8bce
Nick <parikmaher534@gmail.com>
2012-08-27 11:48:31 +0300

    Add base js/css structure

---
+++ scripts/config/configs.js
@@ -0,0 +1,8 @@
+var _J_Config = {
+    canvasId    : "plot",
+    width       : 720,
+    height      : 400,
+    src         : {
+        empty : "resources/empty.png"
+    }
+};
---
+++ scripts/config/lang.js
@@ -0,0 +1,6 @@
+var _J_Lang = {
+  nocanvas  : "No canvas with this ID.",
+  noctx     : "You browser dosen't support CANVAS.",
+  nocache   : "There is no cache object.",
+  example   : "Example"
+};
