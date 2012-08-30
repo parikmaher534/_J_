c587b03f8e97bfb689fa4e90cb6522f5da6125de
Nick <parikmaher534@gmail.com>
2012-08-30 11:34:26 +0300

    Implement controlls module

--- scripts/modules/controlls.js
+++ scripts/modules/controlls.js
@@ -7,6 +7,56 @@
  */
 SmartJ.Controlls = (function(){
   
+  //Check on predefine UP,DOWN,LEFT,RIGHT events
+  var KeySet = function(key, e, callback){
+      switch(key.toLowerCase()){
+        case "up":
+          key = 38; break;
+        case "down":
+          key = 40; break;
+        case "right":
+          key = 39; break;
+        case "left":
+          key = 37; break;
+      };
+      if( key == e.keyCode || String(key).toLowerCase() == String.fromCharCode(e.keyCode).toLowerCase()) callback();
+  };
+  
+  //Key down event
+  var Down = function(key, callback){
+    document.addEventListener("keydown", function(e){ KeySet(key, e, callback); });
+  };
+  
+  //Key up event
+  var Up = function(key, callback){
+    document.addEventListener("keyup", function(e){ KeySet(key, e, callback); });
+  };
+  
+  //Key press event
+  var Press = function(key, callback){
+    document.addEventListener("keypress", function(e){ KeySet(key, e, callback); });
+  };
+  
+  return {
+    /**
+     * On key down event
+     * @param key Key event name
+     * @param callback Callback function
+     */
+    KeyDown: function(key, callback){ Down(key, callback); },
+    /**
+     * On key up event
+     * @param key Key event name
+     * @param callback Callback function
+     */
+    KeyUp: function(key, callback){ Up(key, callback); },
+    /**
+     * On key press event
+     * @param key Key event name
+     * @param callback Callback function
+     */
+    KeyPress: function(key, callback){ Press(key, callback); }
+  };
 });
 
 
