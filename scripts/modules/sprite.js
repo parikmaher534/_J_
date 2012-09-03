f35f0a51339067aaa573ab6fe2e306260981cbcc
Nick <parikmaher534@gmail.com>
2012-09-03 15:48:41 +0300

    Add sprite module file

---
+++ scripts/modules/sprite.js
@@ -0,0 +1,66 @@
+/**
+ * Sprite creation module
+ * Module use Timer module
+ */
+SmartJ.Sprite = (function(o, g){
+  
+  "use strict";
+  
+  var lib  = SmartJ,
+      name = "sprite_" + new Date().getTime(),
+      step = 0,
+      obj  = {
+        src: o.src, 
+        sx: o.sx, 
+        sy: o.sy, 
+        sWidth: o.width, 
+        sHeight: o.height, 
+        dx: o.x,
+        dy: o.y, 
+        dWidth: o.width, 
+        dHeight: o.height,
+        stage: o.stage,
+        
+        //Additional
+        frames: o.frames,
+        speed: o.speed,
+        vector: o.vector
+      },
+      sprite = lib.Create.Object("frame", obj, g);
+  
+  
+  var CalcFrames = function(sprite, s, g){
+    if( sprite[s] == sprite[g] * (sprite.frames - 1) ) {
+      sprite[s] = 0;
+      step = 0;
+    };
+    if( step >= sprite[g] ){
+      sprite[s] += sprite[g];
+      step = 0;
+    };
+  };
+  
+  
+  //Add sprite to timer stack
+  lib.Timer().start({
+    name: name,
+    action: function(){
+      
+      //Change frames by speed
+      step += sprite.speed;
+
+      //Set sprite animation vector
+      sprite.vector == "gorizontal" ? CalcFrames(sprite, "sx", "dWidth") : CalcFrames(sprite, "sy", "dHeight");
+    }
+  });
+  
+  
+  /**
+   * Sprite API
+   */
+  return {  
+    Start: function(){ },
+    Stop: function(){ }
+  };
+});
+
