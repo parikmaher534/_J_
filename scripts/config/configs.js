acbbcf85196d932bc2ed7f01c828a48d69d16001
Nick <parikmaher534@gmail.com>
2012-09-07 13:47:48 +0300

    Clear code

--- scripts/config/configs.js
+++ scripts/config/configs.js
@@ -3,22 +3,12 @@ var SmartJ_Config = {
     stages: {
       "map": {
         width: 800,
-        height: 600,
+        height: 500,
         to: "#Game"
       }
     },
     
     images : {
-        hero : "images/test.png",
-        bg1 : "images/bg1.jpg",
-        bg2 : "images/bg2.jpg",
-        bg3 : "images/bg3.jpg",
-        bg4 : "images/bg4.jpg",
-        bg5 : "images/bg5.jpg",
-        bg6 : "images/bg6.jpg",
-        bg7 : "images/bg7.jpg",
-        barbarian : "images/barbarian.png",
-        br_mv: "images/barb_mv.png"
-    }
-    
+        object : "images/test.png",
+    }    
 };
